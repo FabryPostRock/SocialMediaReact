152624fa3ef1c6416c1e3a680b8a4f1b656d91e4
fabri <64443048+FabryPostRock@users.noreply.github.com>
2026-07-29 12:37:25 +0200

    styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
       <Link to="/login">Login</Link>
       <div>
         <p> {auth.currentUser?.displayName}</p>
-        <img src={auth.currentUser?.photoURL || ''} width="100" height="100" />
+        <img src={auth.currentUser?.photoURL || ''} width="20" height="20" />
       </div>
     </div>
   );
