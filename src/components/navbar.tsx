c9ab8a5fbad4b3e14361e7a5fbcd7d4df6fd6f62
fabri <64443048+FabryPostRock@users.noreply.github.com>
2026-07-29 11:56:04 +0200

    sign in with google

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div>
       <Link to="/">Home</Link>
-      <Link to="/">Login</Link>
+      <Link to="/login">Login</Link>
     </div>
   );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,16 @@
+import { auth, provider } from '../config/firebase';
+import { signInWithPopup } from 'firebase/auth';
+
 export const Login = () => {
-  return <div>Login Page</div>;
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+  };
+
+  return (
+    <div>
+      <p> Sign in with Goole To Continue</p>
+      <button onClick={signInWithGoogle}> Sign in With Google</button>
+    </div>
+  );
 };
