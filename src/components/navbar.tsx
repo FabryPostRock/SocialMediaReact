cfdfb2b2bef359f2acd92def897634f764608e0b
fabri <64443048+FabryPostRock@users.noreply.github.com>
2026-07-29 12:19:27 +0200

    get user data from google auth

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom';
+import { auth } from '../config/firebase';
 
+/*
+<img src={auth.currentUser?.photoURL || ''} /> : Se la foto non è presente 
+Typescript non accetta null e obbliga a settare una stringa vuota come alternativa.
+*/
 export const Navbar = () => {
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
+      <div>
+        <p> {auth.currentUser?.displayName}</p>
+        <img src={auth.currentUser?.photoURL || ''} width="100" height="100" />
+      </div>
     </div>
   );
 };
