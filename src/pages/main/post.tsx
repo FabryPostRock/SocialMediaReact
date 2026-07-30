eaa9147de4b71a6c0a921b3ba4aff82bf7bf7345
fabri <64443048+FabryPostRock@users.noreply.github.com>
2026-07-30 17:09:26 +0200

    remove like and update them seamlessly

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,4 +1,5 @@
-import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+// doc: specifies a specific doc u want to retrive
+import { addDoc, collection, getDocs, deleteDoc, doc, query, where } from 'firebase/firestore';
 import { Post as IPost } from './main';
 import { auth, db } from '../../config/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -9,6 +10,7 @@ interface Props {
 }
 
 interface Like {
+  likeId: string;
   userId: string;
 }
 
@@ -26,10 +28,38 @@ export const Post = (props: Props) => {
   const likesDoc = query(likesRef, where('postId', '==', post.id));
   const getLikes = async () => {
     const data = await getDocs(likesDoc);
-    setLikes(data.docs.map((doc) => ({ userId: doc.data().userId })));
+    setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })));
   };
   const addLike = async () => {
-    await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+    try {
+      const newDoc = await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+      // aggiorno lo state dei Likes in modo immediato aggiungendo un nuovo userId che ha messo like.
+      if (user) {
+        setLikes((prev) =>
+          prev ? [...prev, { userId: user?.uid, likeId: newDoc.id }] : [{ userId: user?.uid, likeId: newDoc.id }],
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const removeLike = async () => {
+    try {
+      // the result of the query below must be 1 so we can staticly point to 'likeToDeleteData.docs[0].id'
+      const likeToDeleteQuery = query(likesRef, where('postId', '==', post.id), where('userId', '==', user?.uid));
+      const likeToDeleteData = await getDocs(likeToDeleteQuery);
+      const likeId = likeToDeleteData.docs[0].id;
+      const likeToDelete = doc(db, 'likes', likeId);
+
+      await deleteDoc(likeToDelete);
+      // aggiorno lo state dei Likes in modo immediato aggiungendo un nuovo userId che ha messo like.
+      if (user) {
+        setLikes((prev) => prev && prev?.filter((like) => like.likeId !== likeId));
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const hasUserLiked = likes?.find((like) => like.userId === user?.uid);
@@ -48,7 +78,10 @@ export const Post = (props: Props) => {
       <div className="footer">
         <p> {post.username}</p>
         {/*<>&#128078;</> : il fragment è aggiunto per distinguere la parte js da quella html*/}
-        <button onClick={addLike}> {hasUserLiked ? <>&#128078;</> : <>&#128077;</>} </button>
+        <button onClick={hasUserLiked ? removeLike : addLike}>
+          {' '}
+          {hasUserLiked ? <>&#128078;</> : <>&#128077;</>}{' '}
+        </button>
         {/*likeAmount && : Visuliazzo 'Likes:' solo se c'è almeno un like*/}
         {likes && <p> Likes: {likes?.length}</p>}
       </div>
