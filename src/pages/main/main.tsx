4b5bb10bb1eaa2ea1e334136e66b2cb7e7154037
fabri <64443048+FabryPostRock@users.noreply.github.com>
2026-07-30 12:03:57 +0200

    created Post component and query to get post data

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,56 @@
+/* from firestore library we can choose between:
+- getDocs: to get multiple docs
+- getDoc: for a single doc
+*/
+import { collection, doc, documentSnapshotFromJSON, getDocs } from 'firebase/firestore';
+import { db } from '../../config/firebase';
+import { useEffect, useState } from 'react';
+import { Post } from './post';
+
+export interface Post {
+  id: string;
+  userId: string;
+  title: string;
+  username: string;
+  description: string;
+}
+
+/*
+Thanks to firebase it's not necessary to use ReactQuery to Query data
+given that firebase has it's own methods.
+*/
+export const Main = () => {
+  // we use useState to save the data we get back from the request
+  const [postsList, setPostsList] = useState<Post[] | null>(null);
+  const postRef = collection(db, 'post');
+  const getPosts = async () => {
+    const data = await getDocs(postRef);
+    setPostsList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]);
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return (
+    <div>
+      {/*
+      Questa scrittura è una arrow function con return implicito: 
+      (post) => (
+        <Post post={post} />
+      )
+      ed è equivalente a :
+      (post) => {
+        return <Post post={post} />;
+      }
+      */}
+      {postsList?.map((post) => (
+        /*
+          <Post post={post} /> : Crea un’istanza del componente React Post e passagli una prop chiamata post, 
+          il cui valore è la variabile post corrente del map().
+          Il post corrente quindi viene passato come prop.
+          Aggiungere sempre la proprietà 'key' per mappare correttamente oggetti iterabili 
+        */
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
